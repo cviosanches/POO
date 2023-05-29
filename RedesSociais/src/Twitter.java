3a9ab915c6ab5e428c9d39728feed3643ad76cf8
cviosanches <134972763+cviosanches@users.noreply.github.com>
2023-05-29 18:27:56 -0300

    Update Twitter.java

--- RedesSociais/src/Twitter.java
+++ RedesSociais/src/Twitter.java
@@ -3,22 +3,22 @@ public class Twitter extends RedeSocial implements Compartilhamento
     @Override
     public void postarFoto() {
 
-        System.out.println("O usuário postou uma foto no Twitter");
+        System.out.println("postou uma foto no Twitter");
     }
 
     @Override
     public void postarVideo() {
-        System.out.println("O usuário postou um vídeo no Twitter");
+        System.out.println("postou um vídeo no Twitter");
 
     }
 
     @Override
     public void postarComentario() {
-        System.out.println("O usuário comentou em uma publicação no Twitter");
+        System.out.println("comentou em uma publicação no Twitter");
     }
 
     @Override
     public void compartilhar() {
-        System.out.println("O usuário compartilhou uma publicação no Twitter");
+        System.out.println("compartilhou uma publicação no Twitter");
     }
 }
